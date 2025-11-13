a5df26bea410a0d39319839a404d777e1b2ae141
mcbarinov <mcbarinov@proton.me>
2025-11-13 09:02:39 +0300

    update

---
+++ eslint.config.js
@@ -0,0 +1,48 @@
+import js from "@eslint/js"
+import globals from "globals"
+import reactHooks from "eslint-plugin-react-hooks"
+import reactRefresh from "eslint-plugin-react-refresh"
+import tseslint from "typescript-eslint"
+import reactX from "eslint-plugin-react-x"
+import reactDom from "eslint-plugin-react-dom"
+import prettier from "eslint-config-prettier"
+import { defineConfig, globalIgnores } from "eslint/config"
+
+export default defineConfig([
+  globalIgnores(["dist"]),
+  {
+    files: ["**/*.{ts,tsx}"],
+    extends: [
+      js.configs.recommended,
+      tseslint.configs.strictTypeChecked,
+      tseslint.configs.stylisticTypeChecked,
+      reactX.configs["recommended-typescript"],
+      reactDom.configs.recommended,
+      reactHooks.configs.flat.recommended,
+      reactRefresh.configs.vite,
+      prettier,
+    ],
+    languageOptions: {
+      ecmaVersion: 2020,
+      globals: globals.browser,
+      parserOptions: {
+        projectService: true,
+      },
+    },
+  },
+  {
+    files: ["**/components/ui/**/*.{ts,tsx}"],
+    rules: {
+      "@typescript-eslint/consistent-type-definitions": "off",
+      "@typescript-eslint/no-unnecessary-condition": "off",
+      "@typescript-eslint/no-unnecessary-template-expression": "off",
+      "@typescript-eslint/no-unnecessary-type-conversion": "off",
+      "@typescript-eslint/no-unsafe-call": "off",
+      "@typescript-eslint/no-unsafe-assignment": "off",
+      "react-refresh/only-export-components": "off",
+      "react-x/no-context-provider": "off",
+      "react-x/no-use-context": "off",
+      "react-x/no-unstable-context-value": "off",
+    },
+  },
+])
